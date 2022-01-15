78153e05c30c21939f7b22105d2f32b4b8c6d406
kyungeun-j <jjaaaaaang22@gmail.com>
2022-01-15 19:16:21 +0900

    add pokemon sort on page pokemonLevelUp

--- static/pokemonLevelUp.js
+++ static/pokemonLevelUp.js
@@ -4,20 +4,22 @@ let levelupBtn = [...getClass('pokemon')]
 
 // default 포켓몬 리스트를 만들어줌
 function listPokemon(type, json) {
+    console.log(json)
     const listDivEle = getClass('listContainer')[0]
     listDivEle.innerHTML = ""
-    if (type === 'list') {
+    if (type === 'list') {// 합성할 포켓몬 1 선택했을 때
         if (listDivEle.classList.contains('filter')) {
             listDivEle.classList.remove('filter')   
         }
-        listDivEle.classList.add('list')   
-    } else if (type === 'filter') {
+        listDivEle.classList.add('list')
+    } else if (type === 'filter') {// 합성할 포켓몬 2 선택했을 때
         if (listDivEle.classList.contains('list')) {
             listDivEle.classList.remove('list')   
         }
         listDivEle.classList.add('filter') 
     }
 
+    // 포켓몬이 없을 떄
     if (Object.keys(json).length == 0) {
         getClass('listContainer')[0].innerText = '합성할 수 있는 포켓몬이 없습니다.'
     } else {
@@ -50,14 +52,21 @@ function listPokemon(type, json) {
     })
     }
     
+    // 포켓몬 선택 시
     levelupBtn = [...getClass('lsItem')]
     levelupBtn.forEach(btn => {
         btn.addEventListener('click', () => {
             const pokemonID = btn.classList[0];
+
+            // 포켓몬 1 선택했을 떄
             if (btn.parentElement.classList[1] === 'list') {
                 selectPokemon('select', pokemonID, json)
-                pokemonFilter(pokemonID, pokemonJSON[pokemonID]['id'], pokemonJSON[pokemonID]['level'])
-            } else if (btn.parentElement.classList[1] === 'filter') {
+                pokemonFilter(pokemonID, json[pokemonID]['id'], json[pokemonID]['level'])
+
+                getID('pokemonSort').style.display = 'none';
+            }
+            // 포켓몬 2 선택했을 때
+            else if (btn.parentElement.classList[1] === 'filter') {
                 selectPokemon('filter', pokemonID, json)
             }
         })
@@ -66,7 +75,7 @@ function listPokemon(type, json) {
 
 listPokemon('list', pokemonJSON)
 
-// 합성할 포켓몬 선택
+// 합성할 포켓몬 선택했을 때 selectContainer에 이미지 넣어주기
 function selectPokemon(type, pokemonID, json) {
     let second = 0;
     const pokemonSelDivEle = document.createElement('div');
@@ -122,9 +131,12 @@ function selectPokemon(type, pokemonID, json) {
     // 다시 고르기 버튼
     getClass('reSelect')[0].addEventListener('click', (e) => {
         first = pokemonID
-        listPokemon('list', pokemonJSON)
+        // listPokemon('list', pokemonJSON)
         getClass('selectPokemon')[0].innerHTML = ""
         getClass('filterPokemon')[0].innerHTML = ""
+
+        getID('pokemonSort').style.display = 'block';
+        sortPokemon(getID('pokemonSort').value)
     })
 
     // 합성하기 버튼 -> post
@@ -172,4 +184,26 @@ function pokemonFilter(selectPokemon, id, level) {
     })
 
     listPokemon('filter', filterJSON)
+}
+
+// 포켓몬 리스트 정렬
+getID('pokemonSort').addEventListener('change', sortPokemon)
+
+function sortPokemon(e) {
+    const value = e.target === undefined ? getID('pokemonSort').value : e.target.value
+    let json = {}
+
+    if (value == 'catchId') {
+        json = pokemonJSON;
+    } else if (value === 'pokemonId') {
+        json = Object.values(pokemonJSON).sort(function(a, b) {
+            // console.log(a)
+            let x = Number(a['id'])
+            let y = Number(b['id'])
+
+            return (x > y) ? 1 : (x === y) ? 0 : -1;
+        })
+    }
+
+    listPokemon('list', json)
 }
