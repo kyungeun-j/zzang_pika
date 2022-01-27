04b1b3ca49fc9077b5e5d419ae51991fa2fd9a18
kyungeun-j <jjaaaaaang22@gmail.com>
2022-01-28 02:57:02 +0900

    update ui on page pokemonList

--- static/pokemonList.js
+++ static/pokemonList.js
@@ -2,7 +2,7 @@ let pokemonJSON = JSON.parse(getClass('data')[0].innerText);
 let myPokemonJSON = JSON.parse(getClass('myPokemon')[0].innerText);
 
 // 0, 10은 일단 white
-const pokemonListBackground = ['#ffffff', '#87e8ff', '#87fffb', '#87ffc3', '#9eff87', '#d0ff87', '#fffd87', '#ffdb87', '#ffb587', '#ff8080', '#ffffff'];
+// const pokemonListBackground = ['#ffffff', '#87e8ff', '#87fffb', '#87ffc3', '#9eff87', '#d0ff87', '#fffd87', '#ffdb87', '#ffb587', '#ff8080', '#ffffff'];
 
 createPokemons();
 
@@ -18,20 +18,15 @@ function createPokemons() {
         // 이외 포켓몬은 ID 값만 노출
         if (pokemonId in pokemonInfo)
         {
+            console.log(document.location)
+            console.log(pokemonId)
             liEle.style.backgroundImage = 'url(../static/images/' + pokemonId + '.png';
-            liEle.style.backgroundColor = pokemonListBackground[pokemonInfo[pokemonId]['maxLevel']];
-
-            if (pokemonInfo[pokemonId]['maxLevel'] == 10)
-            {
-                // 금색 배경으로 다시 초기화
-            }
-
-
-            // maxPercent는 나중에 업데이트
+            liEle.addEventListener('click', () => location.href = '/pokemonDetail/' + pokemonId);
         }
         else
         {
             liEle.innerText = pokemonId;
+            liEle.classList.add('notIn')
         }
 
         pokemonsUI.appendChild(liEle);
