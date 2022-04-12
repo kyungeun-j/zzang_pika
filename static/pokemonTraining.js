354ecdc12383a3c089e0f302d9e4b74eb35e36a7
kyungeun <jjaaaaaang22@gmail.com>
2022-04-13 06:17:42 +0900

    update layout on page pokemonTraining

--- static/pokemonTraining.js
+++ static/pokemonTraining.js
@@ -1,35 +1,10 @@
 let defaultJSON = JSON.parse(getID('default').innerText.replaceAll("'", '"'));
 const pokemons = JSON.parse(getID('pokemons').innerText.replaceAll("'", '"').replaceAll('None', 'null'));
-const trainingBtn = [...getClass('trainingBtn')];
 
-function setBackgroundColor() {
-    Object.keys(defaultJSON).forEach(pokemons => {
-        const pokemon = getClass(pokemons)[0];
-        pokemon.style.background = '#fbfbfb url(../static/images/'+ defaultJSON[pokemons]['id'] +'.png) no-repeat center';
-    });
-}
-setBackgroundColor();
-
-trainingBtn.forEach(btn => {
-    btn.addEventListener('click', async (e) => {
-        const pokemonID = e.currentTarget.classList[0];
-        const option = new URLSearchParams({
-            field: 'training',
-            pokemonID: pokemonID
-        });
-        
-        const post = await myPokemonPost(option);
-        const data = await post.json();
-        if (!data.msg) {
-            popup_text(data*100 + '% (이)가 되었다!');
-            defaultJSON[pokemonID]['percent'] = data;
-            getClass(pokemonID)[0].children[1].innerText = parseFloat(Math.round(defaultJSON[pokemonID]['percent'] * defaultJSON[pokemonID]['max'])) + " (" + parseInt(Math.round(parseFloat(defaultJSON[pokemonID]['percent']) * 100)) + "%)";
-            setBackgroundColor();
-        } else {
-            popup_text(data.msg);
-        }
-    });
+Object.keys(defaultJSON).forEach(poke => {
+    defaultJSON[poke]['catchID'] = poke;
 });
+pokemon_sort(getID('pokemonSort').value, defaultJSON);
 
 async function myPokemonPost(option) {
     return fetch('/pokemonTraining', {
@@ -43,4 +18,8 @@ async function myPokemonPost(option) {
 }
 
 //common.js
-show_scroll_bar(getID('trainingContainer'));
+getID('pokemonSort').addEventListener('change', (e) => {
+    pokemon_sort(e, defaultJSON);
+});
+
+show_scroll_bar(getClass('listContainer')[0]);
